30ffaade16b6c3d71527e203fafacf808ebb1a51
MartKaal <48913272+MartKaal@users.noreply.github.com>
2020-09-02 10:31:21 +0200

    Testemonials section addes with swiper.js library

--- js/main.js
+++ js/main.js
@@ -9,3 +9,33 @@ hamburger.addEventListener("click", function (e) {
 	overlay.classList.toggle('open');
 	menu.classList.toggle('open')
 })
+
+
+// Swiper
+var mySwiper = new Swiper('.swiper-container', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+	speed: 400,
+	autoplay: {
+		delay: 5000,
+		reverseDirection: true,
+	},
+
+	breakpoints: {
+		// when window width is >= 480px
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 30
+		},
+		764: {
+			slidesPerView: 2,
+			spaceBetween: 30
+		},
+		// when window width is >= 640px
+		1200: {
+			slidesPerView: 3,
+			spaceBetween: 30
+		}
+	}
+})
